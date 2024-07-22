48e0e95068ebd639650b3eae5c423c7e24debc3d
KingAshu22 <ashishprasad9833@gmail.com>
2024-07-22 20:50:37 +0530

    updated ui of collection

--- components/Collections.tsx
+++ components/Collections.tsx
@@ -6,21 +6,21 @@ const Collections = async () => {
   const collections = await getCollections();
 
   return (
-    <div className="flex flex-col items-center gap-10 py-8 px-5">
+    <div className="flex flex-col items-center gap-10 py-4 pr-8">
       <p className="text-heading3-bold">Collections</p>
       {!collections || collections.length === 0 ? (
         <p className="text-body-bold">No collections found</p>
       ) : (
-        <div className="flex flex-wrap items-center justify-center gap-8">
+        <div className="grid grid-cols-2 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-2 items-center justify-center">
           {collections.map((collection: CollectionType) => (
             <Link href={`/collections/${collection._id}`} key={collection._id}>
               <Image
                 key={collection._id}
                 src={collection.image}
                 alt={collection.title}
-                width={150}
-                height={150}
-                className="rounded-xl cursor-pointer"
+                width={200}
+                height={200}
+                className="rounded-lg cursor-pointer"
               />
             </Link>
           ))}
